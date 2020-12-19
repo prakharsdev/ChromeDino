ff18522c763b91a8c628d3668fe99e91addb5e58
Prakhar Srivastava <prakhar@ut.ee>
2020-12-19 23:59:26 +0200

    First Commit

---
+++ Dino.py
@@ -0,0 +1,94 @@
+import pygame
+from pygame.locals import *
+
+x = pygame.init()
+
+screen = pygame.display.set_mode((950, 573))
+pygame.display.set_caption("Chrome Dino")
+message = pygame.font.Font('freesansbold.ttf', 30)
+
+def gameloop():
+    backx = 0
+    backy = 0
+    drawx = 350
+    drawy = 440
+    cactusx = 100
+    cactusy = 430
+    backvelocity = 0
+    walkpoint = 0
+    gravity = 7
+    game = False
+    jump = False
+    gameover = False
+    score = 0
+    while True:
+
+        white = (255,255,255)
+        orange = (255, 100, 100)
+        cactus = pygame.image.load("cactusSmallMany0000.png")
+        cactus = pygame.transform.scale(cactus, (200, 100))
+        cactus1 = pygame.image.load("cactusBig0000.png")
+        cactus1 = pygame.transform.scale(cactus1, (60, 50))
+        cactus2 = pygame.image.load("cactusSmallMany0000.png")
+        cactus2 = pygame.transform.scale(cactus2, (200, 100))
+        dino = pygame.image.load("dinorun0000.png")
+        dino1 = pygame.image.load("dinorun0001.png")
+
+        walk = [dino, dino1, dino, dino1]
+        background = pygame.image.load("un.png")
+        
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+            if event.type == KEYDOWN:
+                if event.key == K_UP:
+                    if drawy == 440:
+                        jump = True
+                        backvelocity = 6
+                        game = True
+                    if event.key == K_SPACE:
+                        gameloop()
+        if backx == -600:
+            backx = 0
+        if cactusx < -1600:
+            cactusx = 550
+        if 441 > drawy > 225:
+            if jump == True:
+                drawy -= 7
+        else:
+            jump = False
+        print(drawy)
+        if drawy < 440:
+            if jump == False:
+                drawy +=gravity
+        if cactusx < drawx + 50 < cactusx +200 and cactusy < drawy + 50 < cactusy + 50 :
+            backvelocity = 0
+            walkpoint = 0
+            game = False
+            gameover = True
+        if game == True:
+            score += 1
+        screen.fill(white)
+        text = message.render("Score: "+ str(score), True, orange)
+        text1 = message.render("GAME OVER", True, (255,255,0))
+        backx -= backvelocity
+        cactusx -= backvelocity
+        
+        screen.blit(background, [backx,backy])
+        screen.blit(background, [backx + 760,backy])
+        screen.blit(text, [700, 100])
+        if gameover == True:
+            screen.blit(text1, [400, 190])
+        screen.blit(walk[walkpoint], [drawx, drawy])
+        
+        if game == True:
+
+            walkpoint +=1
+            if walkpoint > 3:
+                walkpoint = 0
+        screen.blit(cactus, [cactusx,cactusy])
+        screen.blit(cactus1, [cactusx + 800,cactusy - 8])
+        screen.blit(cactus2, [cactusx + 1200,cactusy - 8])
+        pygame.display.update()
+
+gameloop()
